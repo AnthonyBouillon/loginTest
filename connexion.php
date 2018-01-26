a9ab107ce2b17ddfa098c9d7a347d1d0aa90b1c0
Anthony <anthony_bouillon@outlook.fr>
2018-01-26 16:27:26 +0100

    Inscription connexion à mettre au propre

---
+++ connexion.php
@@ -0,0 +1,57 @@
+<?php
+  session_start();
+
+
+ 
+try {
+    $bdd = new PDO("mysql:host=127.0.0.1;dbname=membres;charset=utf8", "root", "789789");
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+if (!empty($_POST['pseudo']) && !empty($_POST['pass'])){
+$pass= $_POST['pass'];
+$pseudo = $_POST['pseudo'];
+// Vérification des identifiants
+$req = $bdd->prepare('SELECT id, pass, pseudo FROM membres WHERE pseudo = \'' . $pseudo . '\' ');
+$req->execute();
+$resultat = $req->fetch();
+
+if (password_verify($pass, $resultat['pass']))
+{
+    $_SESSION['id'] = $resultat['id'];
+    $_SESSION['pseudo'] = $pseudo;
+    echo 'Vous êtes connecté ! ';
+    
+}
+else
+{
+    echo 'Mauvais identifiant ou mot de passe !';
+}
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+        <title>Inscription</title>
+    </head>
+    <body>
+        <h1 class="text-center">FORMULAIRE D'INSCRIPTION</h1><br/><br/>
+        <p><a href="deconnexion.php">deconnexion</a></p>
+        <p><a href="inscription.php">Inscription</a></p>
+        <form method="POST">
+            <label class="control-label col-sm-2" for="pseudo">Pseudo : </label>
+            <input type="text" class="form-control" id="pseudo" name="pseudo" placeholder="Enter username">
+
+            <label class="control-label col-sm-2" for="pwd">Mot de passe : </label>
+            <input type="password" class="form-control" id="pwd" name="pass" placeholder="Enter password">
+
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>  
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ deconnexion.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+
+  session_destroy();
+  echo 'DECO';
+  header('refresh:2,url=connexion.php');
